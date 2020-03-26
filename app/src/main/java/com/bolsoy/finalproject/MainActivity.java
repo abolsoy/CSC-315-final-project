337e77a2d996a78d5926cbbe99b58f1e2005b02b
abolsoy <abolsoy@gmail.com>
2020-03-26 19:46:22 -0400

    GitHub and Project setup

--- app/src/main/java/com/bolsoy/finalproject/MainActivity.java
+++ app/src/main/java/com/bolsoy/finalproject/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package com.bolsoy.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.bolsoy.finalproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
